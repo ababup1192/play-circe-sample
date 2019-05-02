9f9f90ebb2d72bea2de896be869ad9d264dc14c0
abab <ababup1192@gmail.com>
2019-05-02 13:08:24 +0900

    [GREEN] 100文字より大きいとBAD_REQUESTを返す

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,27 @@
 package controllers
 
-import io.circe.Json
+import io.circe.{Decoder, Json}
 import javax.inject._
 import play.api.libs.circe.Circe
 import play.api.mvc._
 import io.circe.syntax._
 import io.circe.generic.auto._
+import io.tabmo.circe.extra.rules.StringRules
+
 import scala.concurrent.{ExecutionContext, Future}
 
 case class User(id: Int, name: String)
 case class UserCommand(name: String)
+
+object UserCommand {
+  import io.tabmo.json.rules._
+
+  implicit val decoder: Decoder[UserCommand] = Decoder.instance[UserCommand] { c =>
+    for {
+      name <- c.downField("name").read(StringRules.maxLength(100))
+    } yield UserCommand(name)
+  }
+}
 
 @Singleton
 class HomeController @Inject()(
@@ -29,7 +41,12 @@ class HomeController @Inject()(
   }
 
   def addUser(): Action[Json] = Action(circe.json(1024)).async { implicit request =>
-    Future(Created)
+    Future(
+      UserCommand.decoder.decodeJson(request.body) match {
+        case Right(_) => Created
+        case Left(_) => BadRequest("validation error")
+      }
+    )
   }
 
 }
